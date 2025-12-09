27102504da5e29ef14441bee4b80cc240ffa578c
elshankarimi <130746933+elshankarimi@users.noreply.github.com>
2025-12-10 03:18:40 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-  // 💡 حل مشکل صفحه سیاه با استفاده از مسیرهای نسبی (Relative Path)
-  base: './', 
-  
+  // برای Cloudflare Pages معمولاً base: '/' بهترین گزینه است
+  base: '/', 
   build: {
-    outDir: 'dist', 
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: true, // برای دیباگ بهتر
   },
 });
+ 
