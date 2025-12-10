32c04dd3282e941c3b2192f364e6f91d59fe1626
elshankarimi <130746933+elshankarimi@users.noreply.github.com>
2025-12-10 22:44:46 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// اگر نیاز به alias داری (مثلاً برای src یا node_modules)، اینجا اضافه کن
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src', // حالا می‌تونی به جای ../../ از "@/..." استفاده کنی
+    },
+  },
+  server: {
+    port: 3000,
+  },
   build: {
     outDir: 'dist',
-    sourcemap: false
-  }
+  },
 })
