fc25a7b2a41ad7e277fb93f1d3216872bbc7a16b
elshankarimi <130746933+elshankarimi@users.noreply.github.com>
2025-12-11 01:45:44 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: { '@': '/src' },
+    alias: {
+      '@': '/src'
+    }
   },
-  optimizeDeps: {
-    include: [
-      'viem',
-      'wagmi',
-      '@web3modal/wagmi',
-      '@walletconnect/universal-provider'
-    ],
-  },
-  server: { port: 3000 },
-  build: { outDir: 'dist' },
+  build: {
+    outDir: 'dist'
+  }
 })
